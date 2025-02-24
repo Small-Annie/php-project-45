7f0003a2854cbbd314be2821dc2d3664188a8b50
Petrova Anna <petr230999@gmail.com>
2025-02-24 23:56:22 +0300

    Add game "Is the number prime?"

---
+++ src/Games/Prime.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Php\Project\Games\Prime;
+
+use function Php\Project\Engine\playGame;
+
+function isPrime(int $num): bool
+{
+    if ($num === 1) {
+        return false;
+    }
+
+    for ($divider = 2; $divider <= round($num / 2); $divider++) {
+        if ($num % $divider === 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+function playGamePrime(): void 
+{
+    $gameDescription = <<<END
+    Answer "yes" if given number is prime. Otherwise answer "no".  
+    END;
+    $game = function () {
+        $number = rand(1, 100);
+        $correctAnswer = isPrime($number) ? 'yes' : 'no';
+        return [(string) $number, $correctAnswer];
+    };
+
+    playGame($gameDescription, $game);
+}
